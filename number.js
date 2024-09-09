dfa8b2f1b425773fca7714ab4099c254838887a3
Robert05-stack <johnrobertabaya@gmail.com>
2024-09-09 20:09:15 +0800

    initial commit

---
+++ number.js
@@ -0,0 +1,9 @@
+let firstFavNum = 51;
+let secondFavNum = 15;
+
+console.log("Addition:", firstFavNum + secondFavNum);
+console.log("Subtraction:", firstFavNum - secondFavNum);
+console.log("Multiplication:", firstFavNum * secondFavNum);
+console.log("Division:", firstFavNum / secondFavNum);
+consolo.log("Modulos:", firstFavNum % secondFavNum);
+console.log("Exponentiation (Power):", firstFavNum ** secondFavNum);
